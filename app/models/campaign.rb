0424b3365769f0fa4f83ae485c90ba0d02189fb2
bruno bornsztein <bruno.bornsztein@gmail.com>
2014-10-07 16:04:33 -0500

    fix hit rate calculation

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -255,5 +255,11 @@ class Campaign < Smailer::Models::MailCampaign
     deliveries.sum(:complaints_count)
   end
 
+  def hit_rate
+    return nil if deliveries.count == 0
+    opened_mails_count.to_f / deliveries.count
+  end
+
+
 
 end
