2a7bc4b3c521ea930cff4e92f88de7bab9639484
bruno bornsztein <bruno.bornsztein@gmail.com>
2014-11-25 12:28:23 -0600

    get feed enclosure if available

---
+++ spec/models/mailing_list_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe MailingList do
+
+  describe "#import_rows" do
+    let(:subscription) {
+      sub = FactoryGirl.create(:subscription, email: 'foo@bar.com')
+      sub.subscribed = false
+      sub.mailing_lists << FactoryGirl.create(:mailing_list)
+      sub.save!
+      sub
+    }
+
+
+    it "should not change subscribed state of existing subscription" do
+      mailing_list = subscription.mailing_lists.first
+
+      subscription.subscribed.should == false
+
+      rows = [
+        {email: subscription.email, name: 'John Doe'}
+      ]
+      mailing_list.import_rows rows
+
+      subscription.subscribed.should == false
+    end
+
+  end
+
+
+end
