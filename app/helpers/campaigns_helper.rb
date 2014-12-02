9012df15cbf2a1633a6841ec1eabc54177d6121d
bruno bornsztein <bruno.bornsztein@gmail.com>
2014-12-02 12:00:53 -0600

    fix

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,5 +1,14 @@
 module CampaignsHelper
 
+  def entry_image(entry)
+    if entry.image
+      #add protocol to relative protocol images
+      entry.image.starts_with?('//') ? "http:#{entry.image}" : entry.image
+    else
+      first_image(entry.summary)
+    end
+  end
+
   def first_image(html)
     doc = Nokogiri::HTML(html)
     if img = doc.at('img')
