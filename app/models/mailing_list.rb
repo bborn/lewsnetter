11e4911d795f935e45b5b43bf0b580f2d2463dc2
bruno bornsztein <bruno.bornsztein@gmail.com>
2015-04-21 10:26:38 -0500

    fix

--- app/models/mailing_list.rb
+++ app/models/mailing_list.rb
@@ -5,7 +5,7 @@ Heuristics.define(:column_tester) do
   assume(:email_value)  { value.match /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
   assume(:name_key)     { value.match /.*(name|login).*/i }
   assume(:email_key)    { value.match /.*e.*mail.*/i }
-  assume(:created_at_key) { value.match /.*(creat|subscrib).*/i }
+  assume(:created_at_key) { value.match /.*(date|creat|subscrib).*/i }
 end
 
 class MailingList < Smailer::Models::MailingList
